3dafe8a2336ace203efa957f708a46500835b06f
halla7 <32346308+halla7@users.noreply.github.com>
2017-10-14 19:14:40 -0400

    PostfixCalc
    This code is working for me

---
+++ src/StackInterface.java
@@ -0,0 +1,41 @@
+
+public interface StackInterface<T> {
+
+	boolean add(int index, T t);
+	
+	boolean add(T o);
+	
+	void clear();
+	
+	boolean contains(T o);
+	
+	T get(int index);
+	
+	int indexOf(T o);
+	
+	boolean isEmpty();
+	
+	T remove(int index);
+	
+	T remove(T o);
+	
+	boolean set(int index, T element);
+	
+	int size();
+	
+	StackList<T> subList(int fromIndex, int toIndex);
+	
+	T[] toArray();
+	
+	boolean swap(int position1, int position2);
+	
+	boolean shift(int positions);
+	
+	void push(T o);
+	
+	T pop();
+	
+	T first();
+	
+	T next();
+}
