9c9a15f39e911c1accc567ecf4ff48abe38aa348
spencers1 <spencer.sean22@gmail.com>
2017-11-01 12:33:17 -0400

    Update InfixCalculator.java

--- DSALab3/src/InfixCalculator.java
+++ DSALab3/src/InfixCalculator.java
@@ -1,90 +1,89 @@
-
 public class InfixCalculator {
-	
-	StackList stack = new StackList();
-	PostfixCalculator calculator = new PostfixCalculator();
-	
-	public InfixCalculator() {
-		
-	}
-	
-	
-	public String calcInfix(String input) {
-		String postfix = infixToPostfix(input);
-		postfix = calculator.convertToPostfix(postfix);
-		return postfix;
-	
-	}
-	public String infixToPostfix(String input) {
-		String output = "";
-		StringBuilder stringBuilder = new StringBuilder();
-		for (int i = 0; i < input.length(); i++) {
-			char token = input.charAt(i);
-			
-			if (Character.isDigit(token)) {		// if char is an operand add it to the postfix string
-				stringBuilder.append(Character.toString(token).toString());
-//				stack.push(token);
-//				stack.push(Character.getNumericValue(token));
-			}
-			if (stack.first() == null && Character.isDigit(token)) {
-				stack.push(token);
-			}
-			
-			
-			else if (token == '(') {
-				stack.push(token);
-			}
-			else if (token == ')') {
-				while ((Character) stack.first() != '(') {
-					stringBuilder.append(Character.toString(token).toString());
-					stack.pop();
-				}
-			stack.pop();
-			} else if (isOperator(token)) {
-				while(stack.first() != null && (Character) stack.first() != '(' && isLowerPrecedence((Character) stack.first(), token)) {
-					stringBuilder.append(stack.first().toString());
-					stack.pop();
-				}
-				stack.push(token);
-			}
-			while(stack.first() != null) {
-				stringBuilder.append(stack.first().toString());
-				stack.pop();
-			}
-				output = stringBuilder.toString();
-		}
-		return output;
-	}
-	
-	private static boolean isOperator(char op) {
-		if (op == '*' || op == '/' || op == '+' || op == '-') {
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
-	
-	private static boolean isLowerPrecedence(char op1, char op2)
-	{
-	    switch (op1)
-	    {
-	        case '+':
-	        case '-':
-	            return !(op2 == '+' || op2 == '-');
 
-	        case '*':
-	        case '/':
-	            return op2 == '^' || op2 == '(';
+    private MyStack stack;
+    PostfixCalculator calculator = new PostfixCalculator();
 
-	        case '^':
-	            return op2 == '(';
+    public InfixCalculator() {
+        stack = new MyStack();
+    }
 
-	        case '(':
-	            return true;
 
-	        default:
-	            return false;
-	    }
-	}
+    public String calcInfix(String input) {
+        String postfix = infixToPostfix(input);
+        postfix = calculator.convertToPostfix(postfix);
+        return postfix;
+
+    }
+    public String infixToPostfix(String input) {
+        String postfixString = "";
+        for (int i = 0; i < input.length(); i++) {
+            char value = input.charAt(i);
+            if (value == '(') {
+                stack.push('(');
+            } else if (value == ')') {
+                Character operator = (Character) stack.peek();
+                while (!(operator.equals('(')) && !(stack.isEmpty())) {
+                    postfixString += operator.charValue();
+                    stack.pop();
+                    operator = (Character) stack.peek();
+                }
+                stack.pop();
+            } else if (value == '+' || value == '-') {
+                // if stack is empty
+                if (stack.isEmpty()) {
+                    stack.push(value);
+                } else {
+                    Character operator = (Character) stack.peek();
+                    while (!(stack.isEmpty() || operator.equals(new Character('(')) || operator.equals(new Character(')')))) {
+                        stack.pop();
+                        postfixString += operator.charValue();
+                    }
+                    stack.push(value);
+                }
+
+            } else {
+                postfixString += value;
+            }
+        }
+        while (!(stack.isEmpty())) {
+            Character operator = (Character) stack.peek();
+            if (!operator.equals(new Character('('))) {
+                stack.pop();
+                postfixString += operator.charValue();
+            }
+        }
+        return postfixString;
+    }
+
+    private static boolean isOperator(char op) {
+        if (op == '*' || op == '/' || op == '+' || op == '-') {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    private static boolean isLowerPrecedence(char op1, char op2)
+    {
+        switch (op1)
+        {
+            case '+':
+            case '-':
+                return !(op2 == '+' || op2 == '-');
+
+            case '*':
+            case '/':
+                return op2 == '^' || op2 == '(';
+
+            case '^':
+                return op2 == '(';
+
+            case '(':
+                return true;
+
+            default:
+                return false;
+        }
+    }
 }
