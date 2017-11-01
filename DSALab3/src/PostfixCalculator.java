067c29d88c6b2cec7442caae9abdbde0295b3243
spencers1 <spencer.sean22@gmail.com>
2017-11-01 12:34:01 -0400

    Update PostfixCalculator.java

--- DSALab3/src/PostfixCalculator.java
+++ DSALab3/src/PostfixCalculator.java
@@ -1,61 +1,59 @@
-
 public class PostfixCalculator {
 
-	StackList stack = new StackList();
-	
-	public PostfixCalculator() { 
-		
-	}
-	
-	/*
-	 * Operators are +, -, *, /, and ^.
-	 * 3 levels of precedence
-	 * 3 - 
-	 * 2- *, /
-	 */ 
-	public String convertToPostfix(String input) {
-		input = input.replaceAll("\\s+","");
-		String postfix = null;
-		int value1 = 0;
-		int value2 = 0;
-		int result = 0;
-		for (int i = 0; i < input.length(); i++) {
-			char token = input.charAt(i);
-			if (token == '*') {
-				value2 = (int) stack.pop();
-				value1 = (int) stack.pop();
-				result = value1 * value2;
-				stack.push(result);
-			}
-			if (token == '/') {
-				value2 = (int) stack.pop();
-				value1 = (int) stack.pop();
-				result = value1 / value2;
-				stack.push(result);
-			}
-			if (token == '+') {
-				value2 = (int) stack.pop();
-				value1 = (int) stack.pop();
-				result = value1 + value2;
-				stack.push(result);
-			}
-			if (token == '-') {
-				value2 = (int) stack.pop();
-				value1 = (int) stack.pop();
-				result = value1 - value2;
-				stack.push(result);
-			}
-			if(Character.isDigit(token)) {
-				stack.push(Character.getNumericValue(token));
-			} 						
-		
-		}
-			String endResult = stack.pop().toString();
-		
-		return endResult;
-		
-	}
-	
+//    StackList stack = new StackList();
+    MyStack stack = new MyStack();
+    public PostfixCalculator() {
+
+    }
+
+    /*
+     * Operators are +, -, *, /, and ^.
+     * 3 levels of precedence
+     * 3 -
+     * 2- *, /
+     */
+    public String convertToPostfix(String input) {
+        input = input.replaceAll("\\s+","");
+        String postfix = null;
+        int value1 = 0;
+        int value2 = 0;
+        int result = 0;
+        for (int i = 0; i < input.length(); i++) {
+            char token = input.charAt(i);
+            if (token == '*') {
+                value2 = (int) stack.pop();
+                value1 = (int) stack.pop();
+                result = value1 * value2;
+                stack.push(result);
+            }
+            if (token == '/') {
+                value2 = (int) stack.pop();
+                value1 = (int) stack.pop();
+                result = value1 / value2;
+                stack.push(result);
+            }
+            if (token == '+') {
+                value2 = (int) stack.pop();
+                value1 = (int) stack.pop();
+                result = value1 + value2;
+                stack.push(result);
+            }
+            if (token == '-') {
+                value2 = (int) stack.pop();
+                value1 = (int) stack.pop();
+                result = value1 - value2;
+                stack.push(result);
+            }
+            if(Character.isDigit(token)) {
+                stack.push(Character.getNumericValue(token));
+            }
+
+        }
+        String endResult = stack.pop().toString();
+
+        return endResult;
+
+    }
+
 
 }
-
